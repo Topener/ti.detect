24191dd4c32513030647cbe60123c0c49c5bac25
Rene Pot <rene@topener.nl>
2018-10-05 21:39:13 +0200

    initial commit

---
+++ ti.detect.js
@@ -0,0 +1,27 @@
+var hasNotch = false;
+Alloy.CFG.TiDetect = {};
+
+function hasIOSNotch(){
+    if ( 
+        // iPhone X/Xs
+        Ti.Platform.displayCaps.platformWidth === 375 && 
+        Ti.Platform.displayCaps.platformHeight === 812 && 
+        Ti.Platform.displayCaps.logicalDensityFactor === 3) {
+        return true;
+    }
+
+    if ( 
+        // iPhone XR/XR max
+        Ti.Platform.displayCaps.platformWidth === 414 && 
+        Ti.Platform.displayCaps.platformHeight === 896 && 
+        ( Ti.Platform.displayCaps.logicalDensityFactor === 2 || Ti.Platform.displayCaps.logicalDensityFactor === 3) ) {
+        return true;
+    }
+}
+
+function init(){
+    hasNotch = OS_IOS && hasIOSNotch();
+    Alloy.CFG.TiDetect.statusbarHeight = hasNotch ? 44 : 20;
+}
+
+init();
