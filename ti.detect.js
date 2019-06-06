17dbe473678d61eeeb127f338b08bd0e6ac874e8
Rene Pot <rene@topener.nl>
2019-06-06 10:56:34 +0200

    add support for iPhone XR, closes #2

--- ti.detect.js
+++ ti.detect.js
@@ -3,15 +3,15 @@ Alloy.CFG.TiDetect = {};
 
 function hasIOSNotch(){
     if ( 
-        // iPhone X/Xs
+        // iPhone X/Xs/XR
         Ti.Platform.displayCaps.platformWidth === 375 && 
         Ti.Platform.displayCaps.platformHeight === 812 && 
-        Ti.Platform.displayCaps.logicalDensityFactor === 3) {
+        (Ti.Platform.displayCaps.logicalDensityFactor === 3 || Ti.Platform.displayCaps.logicalDensityFactor === 2)) {
         return true;
     }
 
     if ( 
-        // iPhone XR/XR max
+        // iPhone XR max/XR max
         Ti.Platform.displayCaps.platformWidth === 414 && 
         Ti.Platform.displayCaps.platformHeight === 896 && 
         ( Ti.Platform.displayCaps.logicalDensityFactor === 2 || Ti.Platform.displayCaps.logicalDensityFactor === 3) ) {
