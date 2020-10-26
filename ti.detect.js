f92d3f12f123055479e7a4f081e04b6f416dc3a7
Monili Nicolò <nicolo.monili.nico@gmail.com>
2020-10-26 23:38:48 +0100

    Update ti.detect.js
    Added: iPhone 12 Mini, iPhone 12, iPhone 12 Pro, iPhone 12 Pro Max

--- ti.detect.js
+++ ti.detect.js
@@ -4,17 +4,33 @@ Alloy.CFG.TiDetect = {};
 function hasIOSNotch(){
     if ( 
         // iPhone X/Xs/XR
+        // iPhone X/Xs/XR/12 mini
         Ti.Platform.displayCaps.platformWidth === 375 && 
         Ti.Platform.displayCaps.platformHeight === 812 && 
         (Ti.Platform.displayCaps.logicalDensityFactor === 3 || Ti.Platform.displayCaps.logicalDensityFactor === 2)) {
         return true;
     }
-
     if ( 
         // iPhone XR/XS max
         Ti.Platform.displayCaps.platformWidth === 414 && 
         Ti.Platform.displayCaps.platformHeight === 896 && 
         ( Ti.Platform.displayCaps.logicalDensityFactor === 2 || Ti.Platform.displayCaps.logicalDensityFactor === 3) ) {
+        return true;
+    }
+
+    if (
+        // iPhone 12/12 Pro
+        Ti.Platform.displayCaps.platformWidth === 390 &&
+        Ti.Platform.displayCaps.platformHeight === 844 &&
+        (Ti.Platform.displayCaps.logicalDensityFactor === 2 || Ti.Platform.displayCaps.logicalDensityFactor === 3)) {
+        return true;
+    }
+
+    if (
+        // iPhone 12 Pro max
+        Ti.Platform.displayCaps.platformWidth === 428 &&
+        Ti.Platform.displayCaps.platformHeight === 926 &&
+        (Ti.Platform.displayCaps.logicalDensityFactor === 3)) {
         return true;
     }
 }
@@ -24,9 +40,7 @@ function init(){
     Alloy.CFG.TiDetect.statusbarHeight = hasNotch ? 44 : 20;
     Alloy.CFG.TiDetect.hasNotch = hasNotch;
 }
-
 init();
-
 // exporting init function. There is no need to call this 
 // unless you want to re-evaluate properties. (which shouldn't be needed at all)
 module.exports = {
